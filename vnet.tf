92c8ebd02c1cf3f4f84e53d2a60b2a167764ea4b
Krishna <devopswithkrishnareddy@gmail.com>
2025-10-23 18:30:35 +0530

    added tags to vnet

--- vnet.tf
+++ vnet.tf
@@ -3,6 +3,9 @@ resource "azurerm_virtual_network" "name" {
     address_space = ["10.1.0.0/16"]
     resource_group_name = azurerm_resource_group.rg.name
     location = "eastus"
+    tags = {
+      Name = "krishna"
+    }
   
 }
 
