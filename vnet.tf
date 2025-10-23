e249e6467f75475d98a4a910da3fd533b88e1140
Krishna <devopswithkrishnareddy@gmail.com>
2025-10-23 18:21:43 +0530

    added subnet code

--- vnet.tf
+++ vnet.tf
@@ -5,3 +5,11 @@ resource "azurerm_virtual_network" "name" {
     location = "eastus"
   
 }
+
+resource "azurerm_subnet" "name" {
+    name = "subneto1"
+    address_prefixes = ["10.1.1.0/24"]
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.name.name
+  
+}
